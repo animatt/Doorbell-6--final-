169e5e788651f99b86cabed61caf692b36e44bfc
king <kvcrawford@icloud.com>
2016-12-10 14:40:49 -0500

    add button_press script

---
+++ button_press.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+inpin = 17
+
+GPIO.setup(inpin, GPIO.IN)
+
+import time
+
+prev_input = 0
+
+while True:
+	input = GPIO.input(inpin)
+	
+	if not prev_input and input:
+		print 'Button pressed'
+
+	prev_input = input
+
+	time.sleep(5)
+
