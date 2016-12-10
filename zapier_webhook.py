cc784b24d77b715b6c5f41bd02c8414aee667b19
king <kvcrawford@icloud.com>
2016-12-10 09:42:07 -0500

    remove gtime with local time and sys.argv[1] with strftime(...)

--- zapier_webhook.py
+++ zapier_webhook.py
@@ -2,9 +2,9 @@
 import requests, time
 import sys
 import os
-from time import gmtime, strftime
+rrom time import strftime, localtime
 
-message= {'key1': sys.argv[1], 'key2': strftime("%d.%m.%Y")}  # strftime("%l:%M %p on %d.%m.%Y")
+message= {'key1': strftime('%-I:%M %p', localtime()), 'key2': strftime("%d.%m.%Y", localtime())}  # strftime("%l:%M %p on %d.%m.%Y")
 r = requests.post('https://hooks.zapier.com/hooks/catch/1848657/tkfcc4/', data=message)
 # print r.status_code
 print r.content
