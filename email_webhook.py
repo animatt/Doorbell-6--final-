822fb060b3b3a2044933b8458402160128a1599c
king <kvcrawford@icloud.com>
2016-12-10 14:53:17 -0500

    create function definintions for sending email and sms

--- email_webhook.py
+++ email_webhook.py
@@ -2,7 +2,8 @@
 import requests, time
 from time import strftime, localtime
 
-message= {'key1': strftime('%-I:%M %p', localtime()), 'key2': strftime("%d.%m.%Y", localtime())}
-r = requests.post('https://hooks.zapier.com/hooks/catch/1848657/tk79ae/', data=message)
+def triggerEmail:
+	message= {'key1': strftime('%-I:%M %p', localtime()), 'key2': strftime("%d.%m.%Y", localtime())}
+	r = requests.post('https://hooks.zapier.com/hooks/catch/1848657/tk79ae/', data=message)
 # print r.status_code
-print r.content
+# print r.content
--- zapier_webhook.py
+++ zapier_webhook.py
@@ -4,7 +4,8 @@ import sys
 import os
 from time import strftime, localtime
 
-message= {'key1': strftime('%-I:%M %p', localtime()), 'key2': strftime("%d.%m.%Y", localtime())}  # strftime("%l:%M %p on %d.%m.%Y")
-r = requests.post('https://hooks.zapier.com/hooks/catch/1848657/tkfcc4/', data=message)
+def triggerSMS:
+	message= {'key1': strftime('%-I:%M %p', localtime()), 'key2': strftime("%d.%m.%Y", localtime())}  # strftime("%l:%M %p on %d.%m.%Y")
+	r = requests.post('https://hooks.zapier.com/hooks/catch/1848657/tkfcc4/', data=message)
 # print r.status_code
-print r.content
+# print r.content
